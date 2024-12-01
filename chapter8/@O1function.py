9180fb0da1dd8d274936eef43411c3a8a9d095d4
kaverapp <138514864+kaverapp@users.noreply.github.com>
2024-12-01 23:04:07 +0530

    @Day05

---
+++ chapter8/@O1function.py
@@ -0,0 +1,29 @@
+# .1  Basic Function Syntax 
+
+# write a function to calculate and return the square of a number
+
+'''
+def sqr(num):
+    return num*num
+print(sqr(8))
+'''
+
+# .2 function with Multiple Parameters
+
+#Create a function that takes two numbers as parameters and returns their sum
+'''
+def sum(num1,num2):
+    return num1+num2
+print(sum(10,37))
+'''
+
+# .3 Polymorphism in function
+
+#  write a function multiply that multiplies two numbers. but can also accept and multiply strings
+'''
+def multiply(num1,num2):
+    return num1*num2
+print(multiply("hth",37))
+'''
+
+# .4 function returning multiple values
